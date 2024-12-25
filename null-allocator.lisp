83fa2e451181dbc9afa7cacbf35fb789d33a1df1
Yukari Hafner <shinmera@tymoon.eu>
2024-12-25 13:24:57 +0100

    Try to implement total-size, used-space, and minimum-block-size using malloc_info.

--- null-allocator.lisp
+++ null-allocator.lisp
@@ -21,15 +21,50 @@
     (setf (memory-region-size region) new-size)
     region))
 
-;; FIXME: Need mallinfo for total-size/free-space/etc but that requires supporting struct returns.
+(defun malloc-info ()
+  (let ((string (make-array (* 1024 32) :element-type '(unsigned-byte 8))))
+    (with-pointer-to-array-data (ptr string :direction :output)
+      (let ((file (cffi:foreign-funcall "fmemopen" :pointer ptr :size (1- (length string)) :string "w" :pointer)))
+        (unless (cffi:null-pointer-p file)
+          (unwind-protect
+               (when (= 0 (cffi:foreign-funcall "malloc_info" :int 0 :pointer file :int))
+                 (cffi:foreign-funcall "fflush" :pointer file)
+                 (babel:octets-to-string string :end (cffi:foreign-funcall "ftell" :pointer file :long)))
+            (cffi:foreign-funcall "fclose" :pointer file)))))))
+
+(defun find-info-tag (tag info &optional (start 0) from-end)
+  (let ((pos (search (format NIL "<~a " tag) info :start2 start :from-end from-end)))
+    (when pos (+ pos 2 (length tag)))))
+
 (defmethod total-size ((allocator null))
-  (error "Not implemented"))
+  (cond ((cffi:foreign-symbol-pointer "malloc_info")
+         (let* ((info (malloc-info))
+                (pos (find-info-tag "aspace type=\"total\"" info 0 T)))
+           (parse-integer info :start (+ pos (length "size=\"")) :junk-allowed T)))
+        (T
+         (error "Not implemented"))))
 
 (defmethod used-space ((allocator null))
-  (error "Not implemented"))
+  (cond ((cffi:foreign-symbol-pointer "malloc_info")
+         (let* ((info (malloc-info))
+                (pos (find-info-tag "aspace type=\"total\"" info 0 T))
+                (total (parse-integer info :start (+ pos (length "size=\"")) :junk-allowed T)))
+           (- total
+              (loop for pos = (find-info-tag "size" info) then (find-info-tag "size" info pos)
+                    while pos sum (let ((total (search "total=\"" info :start2 pos)))
+                                    (parse-integer info :start (+ total (length "total=\"")) :junk-allowed T))))))
+        (T
+         (error "Not implemented"))))
 
 (defmethod minimum-block-size ((allocator null))
-  (error "Not implemented"))
+  (cond ((cffi:foreign-symbol-pointer "malloc_good_size")
+         (cffi:foreign-funcall "malloc_good_size" :size 1 :size))
+        ((cffi:foreign-symbol-pointer "malloc_info")
+         (loop with info = (malloc-info)
+               for pos = (find-info-tag "size" info) then (find-info-tag "size" info pos)
+               while pos minimize (parse-integer info :start (+ pos (length "from=\"")) :junk-allowed T)))
+        (T
+         1)))
 
 (defmethod maximum-block-size ((allocator null))
   (error "Not implemented"))
